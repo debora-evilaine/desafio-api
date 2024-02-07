27f4968275527193dce41ebf5e87b50a64d09432
debora-evilaine <sousxdebora@gmail.com>
2024-02-07 12:00:05 -0300

    Primeiro commit com lista de locations

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import urllib.request, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_list_locations():
+    url = "https://rickandmortyapi.com/api/location"
+    response = urllib.request.urlopen(url) 
+    locations = response.read()
+    dict = json.loads(locations)
+
+    locations = []
+
+    for location in dict["results"]:
+        location = {
+            "name": location["name"],
+            "type": location["type"],
+            "dimension": location["dimension"]
+        }
+
+        locations.append(location)
+    
+    return {"locations": locations}
+
