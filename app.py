5ca6535219ab612d9a28279b763e16990b905345
debora-evilaine <sousxdebora@gmail.com>
2024-02-07 12:39:15 -0300

    Adicionando template de locations

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 import urllib.request, json
 
 app = Flask(__name__)
 
 @app.route("/")
+def get_list_locations_page():
+    url = "https://rickandmortyapi.com/api/location"
+    response = urllib.request.urlopen(url) 
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template("locations.html", locations=dict["results"])
+
+@app.route("/lista")
 def get_list_locations():
     url = "https://rickandmortyapi.com/api/location"
     response = urllib.request.urlopen(url) 
